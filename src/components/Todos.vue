6f0c0f64fe7b5eae6f376238fdfdfeac7b87a396
shijie liu <shijieliu@shijies-MacBook-Air.local>
2020-08-04 23:44:19 -0700

    added uuid

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="todos">
     <div v-bind:key="todo.id" v-for="todo in todos">
-      <TodoItem v-bind:todo="todo" />
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
     </div>
   </div>
 </template>
